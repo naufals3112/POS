f20286a94624af183693cdf13c8b7263b41c1d53
Muhammad Naufal Shofwan <naufalshofwantea@gmail.com>
2025-06-08 18:29:49 +0700

    Initial commit

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'barcode',
+        'price',
+        'quantity',
+        'status'
+    ];
+
+    public function getImageUrl()
+    {
+        if ($this->image) {
+            return Storage::url($this->image);
+        }
+        return asset('images/img-placeholder.jpg');
+    }
+}
